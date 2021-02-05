a241ae8013f733ba894e468b6b06c07f2a2478ca
Xuan <salman.dang@gmail.com>
2021-02-05 18:08:13 +0800

    Add files via upload

---
+++ Embedding.py
@@ -0,0 +1,232 @@
+from lightgbm.engine import train
+import pandas as pd
+import numpy as np
+import sqlalchemy
+from sklearn.model_selection import train_test_split
+class Embedding:
+    def __init__(self,F,N=45):
+        self.F=F
+        self.N=N
+    #初始化模型参数
+    def initmodel(self,data):
+        user_item=self.transform(data)
+        P=dict()
+        Q=dict()
+        #2021/2/1 change initialization to be proportional to 1/sqrt(F)
+        for user,items in user_item.items():
+            P.setdefault(user,[])
+            P[user]=np.random.rand(self.F)/np.sqrt(self.F)
+            for item in items:
+                Q.setdefault(item,[])
+                Q[item]=np.random.rand(self.F)/np.sqrt(self.F)
+        self.P=P
+        self.Q=Q
+    #负采样
+    def NegativeSampling(self,items,item_pool,ratio):
+        ret=dict()
+        for i in items:
+            ret.setdefault(i,0)
+            ret[i]=1
+        n=0
+        for i in range(0,10*ratio):
+            item=item_pool[np.random.randint(0,len(item_pool)-1)]
+            if item in ret: ##若随机样本已在列表中则跳过
+                continue
+            ret[item]=0
+            n+=1
+            if n>ratio*len(items):
+                break
+        return ret
+    #模型预测
+    def predict(self,user,item):
+        rui=self.sigmoid(np.array(self.P[user]).T @ np.array(self.Q[item]))
+        return rui
+    #拟合模型
+    def fit(self,data,item_pool,neg_ratio=5,iters=5,eta=0.01,lamda=0.01,verbose=True,init_model=None):
+        if init_model is None:
+            self.initmodel(data)
+        else:
+            self.P=init_model.P
+            self.Q=init_model.Q
+            if init_model.F != self.F:
+                print('Dimension does not match, F in the initial model is'+str(init_model.F)+', set F equal to the same value.\n')
+                return 0
+        
+        user_item=self.transform(data=data)
+        self.losses=[]
+        for i in range(iters):
+            loss=0.0
+            for user,items in user_item.items():
+                #user cold start
+                if user not in self.P.keys():
+                    print('New user entry: '+str(user)+'\n')
+                    self.P.setdefault(user,[])
+                    self.P[user]=np.random.rand(self.F)/np.sqrt(self.F)
+                samples=self.NegativeSampling(items,item_pool,neg_ratio)
+                for item,y in samples.items():
+                    #Item cold start
+                    if item not in self.Q.keys():
+                        print('New item entry: '+str(item)+'\n')
+                        self.Q.setdefault(item,[])
+                        self.Q[item]=np.random.rand(self.F)/np.sqrt(self.F)
+                    yhat=self.predict(user,item)
+                    #eui=y-yhat
+                    for f in range(self.F):
+                        p=self.P[user][f]
+                        q=self.Q[item][f]
+                        g1=eta*(q*(yhat-y)+lamda*p)
+                        g2=eta*(p*(yhat-y)+lamda*q)
+                        self.P[user][f]-=g1
+                        self.Q[item][f]-=g2
+                    yhat=self.predict(user,item)
+                    #cross entropy
+                    reg=0.5*lamda*(np.linalg.norm(self.P[user])+np.linalg.norm(self.Q[item]))
+                    loss+=(-(y*np.log(yhat)+(1-y)*np.log(1-yhat))+reg)/(len(samples)*len(user_item))
+                    #print(loss)
+            if verbose:
+                print('='*15+'Iteration '+str(i)+'='*15+'\n')
+                print('cross entropy loss: '+str(loss))
+            self.losses.append(loss)
+            #self.eta*=0.9
+    def sigmoid(self,x):
+        return 1.0/(1+np.exp(-x))
+    def transform(self,data,user_col='user_id',item_col='item_id'):
+        if data is None:
+            return
+        return dict(data.groupby([user_col])[item_col].agg(set))
+
+    def Candidates(self,user):
+        rank=dict()
+        for item,q in self.Q.items():
+            if item not in rank:
+                rank.setdefault(item,0)
+                rank[item]=self.predict(user,item)
+        return dict(sorted(rank.items(),key=lambda x:x[1],reverse=True)[0:self.N])
+
+    def Recall(self,train,test):
+        train,test=self.transform(data=train),self.transform(data=test)
+        hit=0
+        tot=0
+        for user in train.keys():
+            try:
+                tu=test[user]
+            except:
+                continue   
+            rank=self.Recommend(user=user)
+            for item in rank:
+                if item in tu:
+                    hit+=1
+            tot+=len(tu)
+        return hit/tot
+    #Precision
+    def Precision(self,train,test):
+        train,test=self.transform(data=train),self.transform(data=test)
+        hit=0
+        tot=0
+        for user in train.keys():
+            try:
+                tu=test[user]
+            except:
+                continue
+            rank=self.Recommend(user=user)
+            for item in rank:
+                if item in tu:
+                    hit+=1
+            tot+=self.N
+        return hit/tot
+    '''
+    def Coverage(self,train):
+        train=self.transform(data=train)
+        recommend_items=set()
+        items=set()
+        for user in train.keys():
+            for item in train[user]:
+                items.add(item)
+            rank=self.Recommend(user)
+            for item in rank:
+                recommend_items.add(item)
+        return len(recommend_items)/len(items)
+    '''
+    def getEmbeddings(self):
+        ucol=dict()
+        icol=dict()
+        P=pd.DataFrame(self.P).T
+        Q=pd.DataFrame(self.Q).T
+        for col in P.columns:
+            ucol.setdefault(col,'user_feature'+str(col))
+            icol.setdefault(col,'item_feature'+str(col))
+        P=P.rename(columns=ucol).reset_index().rename(columns={'index':'user_id'})
+        Q=Q.rename(columns=icol).reset_index().rename(columns={'index':'item_id'})
+        
+        return P,Q
+    def get_user_embedding(self):
+        ucol=dict()
+        P=pd.DataFrame(self.P).T
+        for col in P.columns:
+            ucol.setdefault(col,'user_feature'+str(col))
+        P=P.rename(columns=ucol).reset_index().rename(columns={'index':'user_id'})
+        return P
+    def get_item_embedding(self):
+        icol=dict()
+        Q=pd.DataFrame(self.Q).T
+        for col in Q.columns:
+            icol.setdefault(col,'item_feature'+str(col))
+        Q=Q.rename(columns=icol).reset_index().rename(columns={'index':'item_id'})
+        return Q
+    def item_embedding_lookup(self,item_ids):
+        Q=self.get_item_embedding()
+        item_feature=Q[Q['item_id'].isin(item_ids)].reset_index(drop=True)
+        #tmp=pd.DataFrame(np.repeat(np.array(user_feature),len(Q),axis=0))
+        #tmp.columns=list(user_feature.columns)
+        #X=pd.concat([tmp,Q],axis=1)
+        return item_feature
+    def user_embedding_lookup(self,user_ids):
+        P=self.get_user_embedding()
+        user_feature=P.loc[P['user_id'].isin(user_ids)].reset_index(drop=True)
+        return user_feature
+
+    def recall_dataset(self,data,is_test=False,test_size=0.2):
+        tmps=[]
+        data['label_prior']=1
+        for user in data['user_id'].unique():
+            tmp=pd.DataFrame()
+            tmp['item_id']=self.Candidates(user).keys()
+            tmp['user_id']=user
+            tmps.append(tmp)
+        recall_df=pd.concat(tmps,axis=0)
+        recall_df=recall_df.merge(data,how='left',on=['user_id','item_id'])
+        #print(recall_df)
+        recall_df['label']=recall_df['label_prior'].fillna(0)
+        #print(recall_df)
+        del recall_df['label_prior']
+
+        item_features=self.item_embedding_lookup(recall_df['item_id'].unique())
+        user_features=self.user_embedding_lookup(recall_df['user_id'].unique())
+        recall_df=recall_df.merge(item_features,how='left',on=['item_id'])
+        recall_df=recall_df.merge(user_features,how='left',on=['user_id'])
+        feature_cols=[col for col in recall_df.columns if col not in ['user_id','item_id','label']]
+        if is_test:
+            recall_df_train,recall_df_test=train_test_split(recall_df,test_size)
+            return recall_df_train.reset_index(drop=True),recall_df_test.reset_index(drop=True)
+        return recall_df
+
+
+'''
+        users=[]
+        items=[]
+        labels=[]
+        for user,sample in datadict.items():
+            for item,label in sample.items():
+                users.append(user)
+                items.append(item)
+                labels.append(label)
+        df=pd.DataFrame({'user_id':users,'item_id':items,'label':labels})
+        P,Q=self.getEmbeddings()
+        temp=pd.merge(df,P,how='inner',on='user_id')
+        dataset=pd.merge(temp,Q,how='inner',on='item_id')
+        idx=np.random.permutation(len(dataset))
+        dataset=dataset.iloc[idx,:].reset_index(drop=True)
+        return dataset
+'''
+
+
---
+++ Embedding_v2.py
@@ -0,0 +1,166 @@
+from lightgbm.engine import train
+import pandas as pd
+import numpy as np
+import sqlalchemy
+from sklearn.model_selection import train_test_split
+class Embedding:
+    #K:embedding向量的维度，N:召回候选集大小
+    def __init__(self,K,N=75):
+        self.K=K
+        self.N=N
+    #初始化模型参数
+    def initmodel(self,X):
+        X=X.melt().rename(columns={'variable':'feature'})
+        embedding_dims=X.shape[0]
+        #embedding vectors
+        embedding_matrix=np.random.randn(embedding_dims,self.K)/np.sqrt(self.K)
+        col_index=['V'+str(i) for i in range(self.K)]
+        embedding_matrix=pd.DataFrame(embedding_matrix,columns=col_index)
+        #linear coefficients
+        W=np.random.randn(embedding_dims)
+        W=pd.DataFrame(W,columns=['W'])
+        
+        
+        self.embedding_matrix=pd.concat([X,embedding_matrix],axis=1)
+        self.W=pd.concat([X,W],axis=1)
+        #self.feature_index=feature_index
+        '''
+        embeddings=dict()
+        w=dict()
+        features=[]
+        embedding_dims=0
+        for col in X.columns:
+            features.append(col)
+            feature_values=set(X[col])
+            embedding_dims+=len(feature_values)
+            embeddings.setdefault(col,{})
+            w.setdefault(col,{})
+            for value in feature_values:
+                embeddings[col].setdefault(value,[])
+                embeddings[col][value]=np.random.randn(self.K)/np.sqrt(self.K)
+                w[col].setdefault(value,0)
+                w[col][value]=np.random.randn(1)/np.sqrt(self.K)
+        self.features=features
+        self.n_features=len(features)
+        self.embeddings=embeddings
+        self.embedding_dims=embedding_dims
+        self.w=w
+        '''
+        
+    #负采样
+    def NegativeSampling(self,X,item_pool,ratio,user_col='user_id',item_col='item_id'):
+        tmps=[]
+        for user_id in X[user_col].unique():
+            tmp=pd.DataFrame()
+            pos_item=X.loc[X[user_col]==user_id,item_col]
+            neg_item=[]
+            n=0
+            for i in range(0,15*ratio):
+                item=item_pool[np.random.randint(0,len(item_pool)-1)]
+                if item in pos_item:
+                    continue
+                neg_item.append(item)
+                n+=1
+                if n>ratio*len(pos_item):
+                    break
+            tmp[item_col]=neg_item
+            tmp[user_col]=user_id
+            tmp['label']=0
+            tmp=tmp.loc[:,[user_col,item_col]]
+            tmps.append(tmp)
+        neg_sample=pd.concat(tmps,axis=0)
+        del tmps
+        other_feature=[col for col in X.columns if col not in [user_col,item_col,'label']]
+        for col in other_feature:
+            neg_sample[col]=X[col]
+        data=pd.concat([X,neg_sample],axis=0)
+        data['label']=data['label'].fillna(0)
+        idx=np.random.permutation(len(data))
+        data=data.iloc[idx,:].reset_index(drop=True)
+        
+        return data
+
+    #模型得分预测
+    def predict(self,X):
+        
+        return p
+    #拟合模型
+    def fit(self,X,item_pool,ratio=5,num_iterations=5,learning_rate=0.01,verbose=True,init_model=None,label_col='label',debug=False):
+        #负采样，标记数据
+        X[label_col]=1
+        X=self.NegativeSampling(X,item_pool,ratio)
+        ys=X[label_col]
+        X.drop(columns=[label_col],inplace=True)
+        #增量训练
+        if init_model is None:
+            self.initmodel(X)
+        else:
+            self.embbedings=init_model.embeddings
+            self.w=init_model.w
+            if init_model.K != self.K:
+                print('Dimension does not match, K in the initial model is'+str(init_model.K)+', set K equal to the same value.\n')
+                return 0
+        #X=X.melt()
+        #X=pd.merge(X,self.embedding_matrix,how='left',on=['feature','value'])
+        self.losses=[]
+        for i in range(num_iterations):
+            loss=0.0
+            for row in X.index:
+                y=ys[row]
+                x=X.iloc[row,:]
+                y_hat=self.predict(x)
+                #gradient of bias b and coefficient w
+                g=y_hat-y
+                #compute gradient of the cross summand
+                for col1 in X.columns:
+                    
+                    for k in range(self.K):
+                        vjk=0
+                        vi=self.embeddings[col1][X.loc[row,col1]][k]
+                        for col2 in X.columns:
+                            vjk+=self.embeddings[col2][X.loc[row,col2]][k]
+    
+                        g2=g*(vjk-vi)
+                        self.w[col1][X.loc[row,col1]]-=learning_rate*g
+                        self.embeddings[col1][X.loc[row,col1]][k]-=learning_rate*g2
+                y_hat=self.predict(x)
+                loss+=(-(y*np.log(y_hat)+(1-y)*np.log(1-y_hat)))/len(X)     
+            if verbose:
+                print('='*15+'Iteration '+str(i)+'='*15+'\n')
+                print('cross entropy loss: '+str(loss))
+            self.losses.append(loss)
+            #self.eta*=0.9
+    def sigmoid(self,x):
+        return 1.0/(1+np.exp(-x))
+    
+    def Candidates(self,user):
+        rank=dict()
+        for item,q in self.Q.items():
+            if item not in rank:
+                rank.setdefault(item,0)
+                rank[item]=self.predict(user,item)
+        return dict(sorted(rank.items(),key=lambda x:x[1],reverse=True)[0:self.N])
+
+    def recall_dataset(self,data,is_test=False,test_size=0.2):
+        tmps=[]
+        data['label_prior']=1
+        for user in data['user_id'].unique():
+            tmp=pd.DataFrame()
+            tmp['item_id']=self.Candidates(user).keys()
+            tmp['user_id']=user
+            tmps.append(tmp)
+        recall_df=pd.concat(tmps,axis=0)
+        recall_df=recall_df.merge(data,how='left',on=['user_id','item_id'])
+        recall_df['label']=recall_df['label_prior'].fillna(0)
+        del recall_df['label_prior']
+
+        item_features=self.item_embedding_lookup(recall_df['item_id'].unique())
+        user_features=self.user_embedding_lookup(recall_df['user_id'].unique())
+        recall_df=recall_df.merge(item_features,how='left',on=['item_id'])
+        recall_df=recall_df.merge(user_features,how='left',on=['user_id'])
+        feature_cols=[col for col in recall_df.columns if col not in ['user_id','item_id','label']]
+        if is_test:
+            recall_df_train,recall_df_test=train_test_split(recall_df,test_size)
+            return recall_df_train.reset_index(drop=True),recall_df_test.reset_index(drop=True)
+        return recall_df
+
