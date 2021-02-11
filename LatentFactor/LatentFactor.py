73c6c4405305164be2705279634dfe1b932b2b16
Xuan DANG <salman.dang@gmail.com>
2021-02-11 13:15:54 +0800

    complete basic lanten factor model

--- LatentFactor/LatentFactor.py
+++ LatentFactor/LatentFactor.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
 import sqlalchemy
+import lightgbm as lgb
+from tqdm import tqdm
 from sklearn.model_selection import train_test_split
-class Embedding:
+class LatentFactor:
     def __init__(self,F,N=45):
         self.F=F
         self.N=N
@@ -52,20 +54,25 @@ class Embedding:
                 return 0
         
         user_item=self.transform(data=data)
+        new_users=0
+        new_items=0
         self.losses=[]
-        for i in range(iters):
+        for i in tqdm(range(iters)):
+            print('\n'+'='*15+'Iteration '+str(i)+'='*15+'\n')
             loss=0.0
             for user,items in user_item.items():
                 #user cold start
                 if user not in self.P.keys():
-                    print('New user entry: '+str(user)+'\n')
+                    #print('New user entry: '+str(user)+'\n')
+                    new_users+=1
                     self.P.setdefault(user,[])
                     self.P[user]=np.random.rand(self.F)/np.sqrt(self.F)
                 samples=self.NegativeSampling(items,item_pool,neg_ratio)
                 for item,y in samples.items():
                     #Item cold start
                     if item not in self.Q.keys():
-                        print('New item entry: '+str(item)+'\n')
+                        #print('New item entry: '+str(item)+'\n')
+                        new_items+=1
                         self.Q.setdefault(item,[])
                         self.Q[item]=np.random.rand(self.F)/np.sqrt(self.F)
                     yhat=self.predict(user,item)
@@ -83,10 +90,11 @@ class Embedding:
                     loss+=(-(y*np.log(yhat)+(1-y)*np.log(1-yhat))+reg)/(len(samples)*len(user_item))
                     #print(loss)
             if verbose:
-                print('='*15+'Iteration '+str(i)+'='*15+'\n')
                 print('cross entropy loss: '+str(loss))
             self.losses.append(loss)
             #self.eta*=0.9
+        print('New user entries: '+str(new_users)+'\n')
+        print('New item entries: '+str(new_items)+'\n')
     def sigmoid(self,x):
         return 1.0/(1+np.exp(-x))
     def transform(self,data,user_col='user_id',item_col='item_id'):
@@ -101,52 +109,7 @@ class Embedding:
                 rank.setdefault(item,0)
                 rank[item]=self.predict(user,item)
         return dict(sorted(rank.items(),key=lambda x:x[1],reverse=True)[0:self.N])
-
-    def Recall(self,train,test):
-        train,test=self.transform(data=train),self.transform(data=test)
-        hit=0
-        tot=0
-        for user in train.keys():
-            try:
-                tu=test[user]
-            except:
-                continue   
-            rank=self.Recommend(user=user)
-            for item in rank:
-                if item in tu:
-                    hit+=1
-            tot+=len(tu)
-        return hit/tot
-    #Precision
-    def Precision(self,train,test):
-        train,test=self.transform(data=train),self.transform(data=test)
-        hit=0
-        tot=0
-        for user in train.keys():
-            try:
-                tu=test[user]
-            except:
-                continue
-            rank=self.Recommend(user=user)
-            for item in rank:
-                if item in tu:
-                    hit+=1
-            tot+=self.N
-        return hit/tot
-    '''
-    def Coverage(self,train):
-        train=self.transform(data=train)
-        recommend_items=set()
-        items=set()
-        for user in train.keys():
-            for item in train[user]:
-                items.add(item)
-            rank=self.Recommend(user)
-            for item in rank:
-                recommend_items.add(item)
-        return len(recommend_items)/len(items)
-    '''
-    def getEmbeddings(self):
+    def get_embeddings(self):
         ucol=dict()
         icol=dict()
         P=pd.DataFrame(self.P).T
@@ -175,9 +138,6 @@ class Embedding:
     def item_embedding_lookup(self,item_ids):
         Q=self.get_item_embedding()
         item_feature=Q[Q['item_id'].isin(item_ids)].reset_index(drop=True)
-        #tmp=pd.DataFrame(np.repeat(np.array(user_feature),len(Q),axis=0))
-        #tmp.columns=list(user_feature.columns)
-        #X=pd.concat([tmp,Q],axis=1)
         return item_feature
     def user_embedding_lookup(self,user_ids):
         P=self.get_user_embedding()
@@ -194,48 +154,66 @@ class Embedding:
             tmps.append(tmp)
         recall_df=pd.concat(tmps,axis=0)
         recall_df=recall_df.merge(data,how='left',on=['user_id','item_id'])
-        #print(recall_df)
         recall_df['label']=recall_df['label_prior'].fillna(0)
-        #print(recall_df)
         del recall_df['label_prior']
 
         item_features=self.item_embedding_lookup(recall_df['item_id'].unique())
         user_features=self.user_embedding_lookup(recall_df['user_id'].unique())
         recall_df=recall_df.merge(item_features,how='left',on=['item_id'])
         recall_df=recall_df.merge(user_features,how='left',on=['user_id'])
-        feature_cols=[col for col in recall_df.columns if col not in ['user_id','item_id','label']]
+        idx=np.random.permutation(len(recall_df))
+        recall_df=recall_df.iloc[idx,:].reset_index(drop=True)
         if is_test:
             recall_df_train,recall_df_test=train_test_split(recall_df,test_size)
             return recall_df_train.reset_index(drop=True),recall_df_test.reset_index(drop=True)
         return recall_df
+    def train_prediction_model(self,train,test=None,init_model=None,init_lgb_model=None):
+        if init_model is not None:
+            self.P=init_model.P
+            self.Q=init_model.Q
+        train=self.recall_dataset(train)
+        cols=[col for col in train.columns if col not in ['user_id','item_id','label']]
+        lgbtrain=lgb.Dataset(train[cols],label=train['label'])
+        clf_params={'objective':'binary','metric':'auc','boosting':'goss','eta':0.05,'num_leaves':12,'max_depth':6,
+        'min_sum_hessian_in_leaf':0.002,'subsample':0.6,'colsample_bytree':0.6,'reg_alpha':0.2,'reg_lambda':0.08,'is_unbalance':True}
+        if test is not None:
+            test=self.recall_dataset(test)
+            lgbtest=lgb.Dataset(test[cols],label=test['label'])
+            lgb_clf=lgb.train(clf_params,lgbtrain,num_boost_round=500,early_stopping_rounds=50,valid_sets=[lgbtrain,lgbtest],
+                            valid_names=['train','test'],verbose_eval=100,init_model=init_lgb_model)
+        else:
+            lgb_clf=lgb.train(clf_params,lgbtrain,num_boost_round=500,early_stopping_rounds=50,valid_sets=[lgbtrain],
+                            valid_names=['train'],verbose_eval=100,init_model=init_lgb_model)
+        self.prediction_model=lgb_clf
+        return lgb_clf
 
+    def feeling_lucky(self,user_ids,topK=5):
+        user_ids=list(user_ids)
+        tmps=[]
+        for user_id in user_ids:
+            try:
+                items=self.Candidates(user_id).keys()
+            except:
+                print('New user entry: '+str(user_id)+'. Initializing latent factors...')
+                self.P.setdefault(user_id,[])
+                self.P[user_id]=np.random.rand(self.F)/np.sqrt(self.F)
+                items=self.Candidates(user_id).keys()
+            tmp=pd.DataFrame()
+            tmp['item_id']=items
+            tmp['user_id']=user_id
+            tmps.append(tmp)
 
-'''
-        users=[]
-        items=[]
-        labels=[]
-        for user,sample in datadict.items():
-            for item,label in sample.items():
-                users.append(user)
-                items.append(item)
-                labels.append(label)
-        df=pd.DataFrame({'user_id':users,'item_id':items,'label':labels})
-        P,Q=self.getEmbeddings()
-        temp=pd.merge(df,P,how='inner',on='user_id')
-        dataset=pd.merge(temp,Q,how='inner',on='item_id')
-        idx=np.random.permutation(len(dataset))
-        dataset=dataset.iloc[idx,:].reset_index(drop=True)
-        return dataset
-'''
-def FM_Model(feature_dim):
-    inputs=tf.keras.layers.Input(shape=(feature_dim,))
-    linear=tf.keras.layers.Dense(1,kernel_regularizer=tf.keras.regularizers.l2(0.01),
-                                bias_regularizer=tf.keras.regularizers.l2(0.01))(inputs)
-    cross=crosslayer(feature_dim)(inputs)
-    add=tf.keras.layers.Add()([linear,cross])
-    pred=tf.keras.layers.Activation('sigmoid')(add)
-    model=tf.keras.Model(inputs=inputs,outputs=pred)
-    model.summary()
-    model.compile(loss='binary_crossentropy',optimizer=tf.keras.optimizers.Adam(0.06),metrics=['AUC'])
-    return model
+        recall_df=pd.concat(tmps,axis=0)
+        recall_df=recall_df.loc[:,['user_id','item_id']]
+        item_features=self.item_embedding_lookup(recall_df['item_id'].unique())
+        user_features=self.user_embedding_lookup(recall_df['user_id'].unique())
+        recall_df=recall_df.merge(item_features,how='left',on=['item_id'])
+        recall_df=recall_df.merge(user_features,how='left',on=['user_id'])
+        y_scores=self.prediction_model.predict(recall_df.drop(columns=['user_id','item_id']))
+        recall_df['pred']=y_scores
+        
+        recall_df['rank']=recall_df.groupby(['user_id'])['pred'].rank(method='first',ascending=False)
+        recmd_df=recall_df.sort_values(by=['user_id','rank']).loc[:,['user_id','item_id','rank']]
+        recmd_df=recmd_df.groupby(['user_id'])['item_id','rank'].apply(lambda x : x[:topK]).droplevel(1).reset_index()
+        return recmd_df
 
